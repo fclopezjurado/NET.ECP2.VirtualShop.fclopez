f87ec83f1f206ddb640282039ea191f90e3896f7
Francisco López <fclopezjurado@gmail.com>
2017-02-28 21:06:07 +0100

    Initial Commit. Francisco Carlos López

---
+++ VirtualShop/VirtualShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VirtualShop.MvcApplication" Language="C#" %>
---
+++ VirtualShop/VirtualShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VirtualShop.Startup))]
+namespace VirtualShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
