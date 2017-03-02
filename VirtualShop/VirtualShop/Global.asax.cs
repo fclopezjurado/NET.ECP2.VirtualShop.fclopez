78924e6a7e01354bef6978c2ad4581b4caef0729
Francisco López <fclopezjurado@gmail.com>
2017-03-02 23:38:35 +0100

    Adding project configuration files and database

--- VirtualShop/VirtualShop/Global.asax.cs
+++ VirtualShop/VirtualShop/Global.asax.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using VirtualShop.Models;
 
 namespace VirtualShop
 {
@@ -16,6 +13,8 @@ namespace VirtualShop
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
+            ModelBinders.Binders.Add(typeof(User), new UserModelBinder());
         }
     }
 }
