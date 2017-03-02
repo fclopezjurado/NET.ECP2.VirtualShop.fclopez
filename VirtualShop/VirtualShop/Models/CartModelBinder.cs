f26a428fe60451b6d298818cb3ba7090002cf956
Francisco López <fclopezjurado@gmail.com>
2017-03-02 23:37:53 +0100

    Adding models

---
+++ VirtualShop/VirtualShop/Models/CartModelBinder.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace VirtualShop.Models
+{
+    public class CartModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            Cart chart = (Cart)controllerContext.HttpContext.Session["chart"];
+
+            if (chart == null)
+            {
+                chart = new Cart();
+                controllerContext.HttpContext.Session["chart"] = chart;
+            }
+
+            return chart;
+        }
+    }
+}
---
+++ VirtualShop/VirtualShop/Models/UserModelBinder.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VirtualShop.Models
+{
+    public class UserModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            User user = (User)controllerContext.HttpContext.Session["user"];
+
+            if (user == null)
+            {
+                user                                            = new User();
+                controllerContext.HttpContext.Session["user"]   = user;
+            }
+
+            return user;
+        }
+    }
+}
